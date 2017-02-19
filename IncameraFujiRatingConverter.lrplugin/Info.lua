56588f2ca8fa473609c0a8b1fffd933b28885fc3
Aldis Ameriks <aldis.ameriks@gmail.com>
2017-02-19 17:36:41 +0200

    Convert tabs to spaces

--- IncameraFujiRatingConverter.lrplugin/Info.lua
+++ IncameraFujiRatingConverter.lrplugin/Info.lua
@@ -1,8 +1,8 @@
 --[[----------------------------------------------------------------------------
 
 ADOBE SYSTEMS INCORPORATED
- Copyright 2007 Adobe Systems Incorporated
- All Rights Reserved.
+Copyright 2007 Adobe Systems Incorporated
+All Rights Reserved.
 
 NOTICE: Adobe permits you to use, modify, and distribute this file in accordance
 with the terms of the Adobe license agreement accompanying it. If you have received
@@ -17,17 +17,17 @@ Info.lua
 
 return {
 
-	LrSdkVersion = 5.0,
-	LrSdkMinimumVersion = 4.0, -- minimum SDK version required by this plug-in
+  LrSdkVersion = 5.0,
+  LrSdkMinimumVersion = 4.0, -- minimum SDK version required by this plug-in
 
-	LrToolkitIdentifier = 'lv.ameriks.lightroom.library.incamerafujiratingconverter',
-	LrPluginName = "Incamera Fuji Rating Converter",
+  LrToolkitIdentifier = 'lv.ameriks.lightroom.library.incamerafujiratingconverter',
+  LrPluginName = "Incamera Fuji Rating Converter",
 
-	LrLibraryMenuItems = {
-		{
-			title = 'Convert Fuji incamera ratings',
-			file = "IncameraFujiRatingConverter.lua"
-		}
-	},
-	VERSION = { major=1, minor=0, revision=0, build=18701234, },
+  LrLibraryMenuItems = {
+    {
+      title = 'Convert Fuji incamera ratings',
+      file = "IncameraFujiRatingConverter.lua"
+    }
+  },
+  VERSION = { major=1, minor=0, revision=0, build=18701234, },
 }
